b257a13536a32e1b3a3a576e457de63910a99281
Zemtsov Igor <zemtsovigor@yahoo.com>
2024-08-03 12:33:45 +0200

    expand disabled

--- src/index.tsx
+++ src/index.tsx
@@ -6,15 +6,15 @@ import {WebSocketProvider} from "./providers/webSocketProvider";
 import App from "./App";
 import store from './store';
 // @ts-ignore
-const DEFAULT_WEBAPP = typeof window !== 'undefined' && window?.Telegram?.WebApp ? window.Telegram.WebApp : null;
+// const DEFAULT_WEBAPP = typeof window !== 'undefined' && window?.Telegram?.WebApp ? window.Telegram.WebApp : null;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
-if (!DEFAULT_WEBAPP.isExpanded) {
-  DEFAULT_WEBAPP.expand();
-}
+// if (!DEFAULT_WEBAPP.isExpanded) {
+//   DEFAULT_WEBAPP.expand();
+// }
 
 root.render(
   <React.StrictMode>
