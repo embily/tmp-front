babb3593277d1de361dd7f0c3f00a62bb7535420
Zemtsov Igor <zemtsovigor@yahoo.com>
2024-08-03 16:51:07 +0200

    removed menu

--- src/components/Layout/Layout.Styles.tsx
+++ src/components/Layout/Layout.Styles.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Container: any = styled.div`
   width: 100%;
   height: 100%;
-  overflow: hidden;
+  //overflow: hidden;
   
   .main {
     &-scrolled {
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -17,63 +17,63 @@ const Menu: React.FC<Props> = () => {
 
   return (
     <MenuContainer className="menu">
-      <ul className="menu-list">
-        <li className={`menu-list__item ${pathname === PATHS.MINE ? '-active' : ''}`}>
-          <NavLink
-            tabIndex={-1}
-            to={PATHS.MINE}
-            className='menu-list__link'
-          >
-            <div className="menu-list__icon">
-              <MineSVG />
-            </div>
-          </NavLink>
-        </li>
-        <li className={`menu-list__item ${pathname === PATHS.UPGRADE ? '-active' : ''}`}>
-          <NavLink
-            tabIndex={-1}
-            to={PATHS.UPGRADE}
-            className='menu-list__link'
-          >
-            <div className="menu-list__icon">
-              <UpgradeSVG/>
-            </div>
-          </NavLink>
-        </li>
-        <li className={`menu-list__item ${pathname === PATHS.ITEMS ? '-active' : ''}`}>
-          <NavLink
-            tabIndex={-1}
-            to={PATHS.ITEMS}
-            className='menu-list__link'
-          >
-            <div className="menu-list__icon">
-              <ItemsSVG/>
-            </div>
-          </NavLink>
-        </li>
-        <li className={`menu-list__item ${pathname === PATHS.FRIENDS ? '-active' : ''}`}>
-          <NavLink
-            tabIndex={-1}
-            to={PATHS.FRIENDS}
-            className='menu-list__link'
-          >
-            <div className="menu-list__icon">
-              <FriendsSVG/>
-            </div>
-          </NavLink>
-        </li>
-        <li className={`menu-list__item ${pathname === PATHS.EARN ? '-active' : ''}`}>
-          <NavLink
-            tabIndex={-1}
-            to={PATHS.EARN}
-            className='menu-list__link'
-          >
-            <div className="menu-list__icon">
-              <EarnSVG/>
-            </div>
-          </NavLink>
-        </li>
-      </ul>
+      {/*<ul className="menu-list">*/}
+      {/*  <li className={`menu-list__item ${pathname === PATHS.MINE ? '-active' : ''}`}>*/}
+      {/*    <NavLink*/}
+      {/*      tabIndex={-1}*/}
+      {/*      to={PATHS.MINE}*/}
+      {/*      className='menu-list__link'*/}
+      {/*    >*/}
+      {/*      <div className="menu-list__icon">*/}
+      {/*        <MineSVG />*/}
+      {/*      </div>*/}
+      {/*    </NavLink>*/}
+      {/*  </li>*/}
+      {/*  <li className={`menu-list__item ${pathname === PATHS.UPGRADE ? '-active' : ''}`}>*/}
+      {/*    <NavLink*/}
+      {/*      tabIndex={-1}*/}
+      {/*      to={PATHS.UPGRADE}*/}
+      {/*      className='menu-list__link'*/}
+      {/*    >*/}
+      {/*      <div className="menu-list__icon">*/}
+      {/*        <UpgradeSVG/>*/}
+      {/*      </div>*/}
+      {/*    </NavLink>*/}
+      {/*  </li>*/}
+      {/*  <li className={`menu-list__item ${pathname === PATHS.ITEMS ? '-active' : ''}`}>*/}
+      {/*    <NavLink*/}
+      {/*      tabIndex={-1}*/}
+      {/*      to={PATHS.ITEMS}*/}
+      {/*      className='menu-list__link'*/}
+      {/*    >*/}
+      {/*      <div className="menu-list__icon">*/}
+      {/*        <ItemsSVG/>*/}
+      {/*      </div>*/}
+      {/*    </NavLink>*/}
+      {/*  </li>*/}
+      {/*  <li className={`menu-list__item ${pathname === PATHS.FRIENDS ? '-active' : ''}`}>*/}
+      {/*    <NavLink*/}
+      {/*      tabIndex={-1}*/}
+      {/*      to={PATHS.FRIENDS}*/}
+      {/*      className='menu-list__link'*/}
+      {/*    >*/}
+      {/*      <div className="menu-list__icon">*/}
+      {/*        <FriendsSVG/>*/}
+      {/*      </div>*/}
+      {/*    </NavLink>*/}
+      {/*  </li>*/}
+      {/*  <li className={`menu-list__item ${pathname === PATHS.EARN ? '-active' : ''}`}>*/}
+      {/*    <NavLink*/}
+      {/*      tabIndex={-1}*/}
+      {/*      to={PATHS.EARN}*/}
+      {/*      className='menu-list__link'*/}
+      {/*    >*/}
+      {/*      <div className="menu-list__icon">*/}
+      {/*        <EarnSVG/>*/}
+      {/*      </div>*/}
+      {/*    </NavLink>*/}
+      {/*  </li>*/}
+      {/*</ul>*/}
     </MenuContainer>
   );
 };
