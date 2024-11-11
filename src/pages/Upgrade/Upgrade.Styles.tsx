5534bef6b03db07fe5c07c10f07babcf6cb70e08
Zemtsov Igor <zemtsovigor@yahoo.com>
2024-11-11 21:18:58 +0000

    fixed text size

--- src/pages/Upgrade/Upgrade.Styles.tsx
+++ src/pages/Upgrade/Upgrade.Styles.tsx
@@ -220,7 +220,7 @@ export const CardsWrap: any = styled.div`
         line-height: 3.7vmin;
         letter-spacing: -0.30000001192092896px;
         color: #FFFFFF;
-        word-break: break-all;
+        word-break: break-word;
       }
 
       &__level {
